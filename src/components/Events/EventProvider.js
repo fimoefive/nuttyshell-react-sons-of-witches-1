00bfe332cf5e78b8ef388567c82ff9ec098ae09f
fimoefive <brothermj@hotmail.com>
2020-10-09 14:53:10 -0500

    EventProvider.js getfetch calls and return EventContext.Provider value.

---
+++ src/components/Events/EventProvider.js
@@ -0,0 +1,32 @@
+import React, { useState, createContext } from 'react';
+
+export const EventContext = createContext()
+
+export const EventProvider = (props) => {
+    const [events, setEvents] = useState([])
+
+    const getEvents = () => {
+        return fetch('http://localhost:8088/events?_expand=location')
+        .then(res => res.json())
+        .then(setEvents)
+    }
+
+    const addEvent = (events) => {
+        return fetch('http://localhost:8088/events', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(events)
+        })
+        .then(getEvents)
+    }
+
+    return (
+        <EventContext.Provider value = {{
+            events, getEvents, addEvent
+        }}>
+            {props.children}
+        </EventContext.Provider>
+    )
+};
